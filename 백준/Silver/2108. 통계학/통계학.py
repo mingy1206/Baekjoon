ef19608187475fec27b411c54b4acb1f892aa374
Lee Min Gi <57743780+mingy1206@users.noreply.github.com>
2025-01-04 22:02:39 +0900

    [Silver III] Title: 통계학, Time: 348 ms, Memory: 145292 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+lst = []
+v = [0]*8001
+total = 0
+for _ in range(n):
+    num = int(input())
+    total += num
+    v[num+4000] += 1
+    lst.append(num)
+
+max_num = max(v)
+lst2 = []
+for i in range(len(v)):
+    if v[i] == max_num:
+        lst2.append(i-4000)
+
+lst.sort()
+lst2.sort()
+
+if n == 1:
+    print(lst[0])
+    print(lst[0])
+    print(lst2[0])
+    print(0)
+else:
+    print(round(total / n))
+    print(lst[int(len(lst)/2)])
+    if len(lst2) >= 2:
+        print(lst2[1])
+    else:
+        print(lst2[0])
+    print(lst[-1]-lst[0])
