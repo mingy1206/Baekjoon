55b9d13f0ae56eea12dbb288066a97069e70fb83
Lee Min Gi <57743780+mingy1206@users.noreply.github.com>
2026-04-10 00:13:00 +0900

    [Silver II] Title: 랜선 자르기, Time: 432 ms, Memory: 21076 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,43 +1,75 @@
 import java.io.BufferedReader;
+
 import java.io.IOException;
+
 import java.io.InputStreamReader;
+
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        
-        // 1. 배열의 크기는 우리가 '가진' 랜선의 개수인 K로 해야 합니다.
-        int[] lens = new int[K]; 
 
-        for(int i = 0; i < K; i++) lens[i] = Integer.parseInt(br.readLine());
-        
-        long high = (long) Math.pow(2,32);
-        long low = 1; // 2. 0으로 나누는 에러 방지를 위해 최소 길이는 1로 시작
-        long max_size = 0;
-        
-        while (true){
-            long mid = (high+low)/2;
-            long num = 0;
-            
-            // 3. 자를 때도 '가진' 랜선 K개만 잘라야 합니다.
-            for(int i = 0; i < K; i++){ 
-                num += lens[i]/mid;
-            }
-            
-            if(num < N) {
-                high = mid-1;
-            } else {
-                max_size = Math.max(max_size, mid);
-                low = mid+1;
-            }
-            
-            // 4. low와 high가 같을 때도 꼭 검사해야 하므로 '>' 일 때 break 합니다.
-            if(low > high) break; 
-        }
-        System.out.println(max_size);
-    }
+
+public class Main {
+
+public static void main(String[] args) throws IOException {
+
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+StringTokenizer st = new StringTokenizer(br.readLine());
+
+int K = Integer.parseInt(st.nextToken());
+
+int N = Integer.parseInt(st.nextToken());
+
+int[] lens = new int[N];
+
+
+
+for(int i = 0; i < K; i++) lens[i] = Integer.parseInt(br.readLine());
+
+long high = (long) Math.pow(2,32);
+
+long low = 0;
+
+long max_size = 0;
+
+while (low <= high){
+
+long mid = (high+low)/2;
+
+long num = 0;
+
+for(int i = 0; i < N; i++){
+
+num += lens[i]/mid;
+
+}
+
+if(num < N) {
+
+high = mid-1;
+
+}else {
+
+max_size = Math.max(max_size, mid);
+
+low = mid+1;
+
+}
+
+// System.out.println("high: "+ high);
+
+// System.out.println("low: "+ low);
+
+// System.out.println("mid: "+ mid);
+
+// System.out.println("`````````````````");
+
+
+
+}
+
+System.out.println(max_size);
+
+}
+
 }
